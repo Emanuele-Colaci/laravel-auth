7c038d2e997b289dc384e01ab080075c33bbdf28
Emanuele-colaci <emanuele.colaci@gmail.com>
2023-08-03 18:00:35 +0200

    Fatta migrate e seeder

--- database/migrations/2023_08_03_155123_create_projects_table.php
+++ database/migrations/2023_08_03_155123_create_projects_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('projects', function (Blueprint $table) {
             $table->id();
+            $table->string('titolo', 50);
+            $table->text('descrizione');
+            $table->date('data');
             $table->timestamps();
         });
     }
